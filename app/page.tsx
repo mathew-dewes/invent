a7c5a04a93521f46ee50b9b16ba18359fc33792f
Mathew Dewes <mathewdewes01@gmail.com>
2025-12-09 14:14:57 +1300

    Initial page routes and navbar added

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,8 @@
 export default function page() {
   return (
-    <div className="mx-10">
-      <div className="mt-10">
-        <div>
-          <h1>Heading 1</h1>
-          <h2>Heading 2</h2>
-          <h3>Heading 3</h3>
-        </div>
-        
+    <div>
+      <h1>Dashboard goes here</h1>
+     
         <div className="mt-10">
    <p>Para: Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet, illum.</p>
         <div className="flex mt-10 gap-10">
@@ -19,7 +14,7 @@ export default function page() {
 
      
 
-      </div>
+     
 
     </div>
   )
---
+++ components/NavLinks.tsx
@@ -0,0 +1,16 @@
+"use client"
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+export default function NavLinks(){
+          const path = usePathname();
+    return (
+         <ul className="flex gap-5 items-center text-white/80">
+                    <Link className={`${path.startsWith("/inventory") ? "text-white font-semibold" : "hover:text-white"}`} href={'/inventory'}>Inventory</Link>
+                    <Link className={`${path.startsWith("/orders") ? "text-white font-semibold" : "hover:text-white"}`} href={'/orders'}>Orders</Link>
+                    <Link className={`${path.startsWith("/shipments") ? "text-white font-semibold" : "hover:text-white"}`}  href={'/shipments'}>Shipments</Link>
+
+                </ul>
+    )
+}
